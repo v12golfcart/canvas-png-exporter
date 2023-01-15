6eccd54c2c2db0e5bbef78d508fd3cba26007c35
Chris Ramesh <chris.ramesh@gmail.com>
2023-01-14 16:22:58 -0800

    2 texts, and an uploaded image exported as png

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const canvasRef = useRef(null);
+  const [uploadedImgUrl, setUploadedImgUrl] = useState();
+
+  const handleCanvas = () => {
+    console.log("handlecanvas", uploadedImgUrl);
+    const ctx = canvasRef.current.getContext("2d");
+    ctx.font = "48px serif";
+    ctx.fillStyle = "blue";
+    ctx.fillText("Hello World", 10, 50);
+    ctx.fillStyle = "green";
+    ctx.fillText("Goodbye World", 10, 100);
+
+    if (uploadedImgUrl) {
+      const img = new Image();
+      img.src = uploadedImgUrl;
+      img.onload = () => ctx.drawImage(img, 10, 150);
+    }
+  };
+
+  const handleImageUpload = (e) => {
+    if (e && e.target.files) {
+      const file = e.target.files[0];
+      setUploadedImgUrl(URL.createObjectURL(file));
+    }
+  };
+
+  const handleExportClick = () => {
+    const canvas = canvasRef.current;
+    canvas.toBlob((blob) => {
+      const url = URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = "canvas.png";
+      a.click();
+      URL.revokeObjectURL(url);
+    }, "image/png");
+  };
+
+  // const handleExportClick = () => {
+  //   const canvas = canvasRef.current;
+  //   const dataUrl = canvas.toDataURL("image/png");
+  //   fetch(dataUrl)
+  //     .then((res) => res.blob())
+  //     .then((blob) => {
+  //       const url = URL.createObjectURL(blob);
+  //       const a = document.createElement("a");
+  //       a.href = url;
+  //       a.download = "canvas.png";
+  //       a.click();
+  //       URL.revokeObjectURL(url);
+  //     });
+  // };
+
+  useEffect(() => {
+    if (canvasRef.current) {
+      handleCanvas();
+    }
+  }, [uploadedImgUrl]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="file" onChange={handleImageUpload} />
+      <button onClick={handleExportClick}>Export as PNG</button>
+      <canvas ref={canvasRef} width={window.innerWidth} height="500" />
     </div>
   );
 }
